0f9585a7922b578392a2918c67431d292633f12a
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-14 18:02:46 +0100

    number is divisible by 5

--- spec/javabuzzSpec.js
+++ spec/javabuzzSpec.js
@@ -1,18 +1,23 @@
 describe('Javabuzz', function(){
 
-	var javabuzz
+  var javabuzz
 
-	describe('should be divisible by', function (){
+  describe('should be divisible by', function (){
 
-		it('fifteen', function(){
-			javabuzz = new Javabuzz();
-			expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
-		});
+	it('fifteen', function(){
+		javabuzz = new Javabuzz();
+		expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+	});
 
+	it('five', function(){
+  		javabuzz = new Javabuzz();
+ 		 expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+  	});
 
-    it('five', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
-	  });
+  	it('', function (){
+  		javabuzz = new Javabuzz();
+  		expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+  	});
+
   });
 });
--- src/javabuzz.js
+++ src/javabuzz.js
@@ -1,5 +1,9 @@
 function Javabuzz() {}
 
 Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-  return true
+  return (number % 15 === 0);
+};
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+	return (number % 5 === 0);
 };
