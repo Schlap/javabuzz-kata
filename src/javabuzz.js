ee8a61d6f8e881653959823c69156389638429da
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-14 18:15:38 +0100

    meh

--- src/javabuzz.js
+++ src/javabuzz.js
@@ -11,3 +11,7 @@ Javabuzz.prototype.isDivisibleByFive = function(number) {
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0)
 };
+
+Javabuzz.prototype.say = function(number) {
+	if(this.isDivisibleByFifteen(15)) return "Javabuzz"
+};
